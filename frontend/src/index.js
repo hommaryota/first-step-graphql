98832ccb70b1e1066effa49d85ab6bfffd6671ff
hommaryota <ryo4111016@gmail.com>
2024-09-24 23:41:23 +0900

    graphqlの簡易的な設定

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+const BOOKS = gql`
+  query Books {
+    books {
+      title
+      author
+    }
+  }
+`;
+
+function Books() {
+  const { loading, error, data } = useQuery(BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error...</p>;
+
+  return data.books.map(({ title, author }) => (
+    <div key={title}>
+      <p>
+        {title} by {author}
+      </p>
+    </div>
+  ));
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <Books />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
